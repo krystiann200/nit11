40cc36112ab60e63ae6c9f5020e5d67163233603
krystiann200 <153029660+krystiann200@users.noreply.github.com>
2023-12-05 22:37:09 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -15,22 +15,23 @@ let nit1 = sprites.create(img`
     ffffffb1ddbffffccbbbbbcf
     fffffb11ddbfffffbcccccff
     fffffb1ddbbfffffbddbffff
-    `, SpriteKind.Projectile)
+    `, SpriteKind.Player)
 let nit2 = sprites.create(img`
-    . . . . . . b b b b . . . . . . 
-    . . . . b b 3 3 3 3 b b . . . . 
-    . . . c b 3 3 3 3 1 1 b c . . . 
-    . . c b 3 3 3 3 3 1 1 1 b c . . 
-    . c c 1 1 1 3 3 3 3 1 1 3 c c . 
+    f f f f f f b b b b f f f f f f 
+    f f f f b b 3 3 3 3 b b f f f f 
+    f f f f b 3 3 3 3 1 1 b c f f f 
+    f f c b 3 3 3 3 3 1 1 1 b c f f 
+    f c c 1 1 1 3 3 3 3 1 1 3 c c f 
     c c d 1 1 1 3 3 3 3 3 3 3 b c c 
     c b d d 1 3 3 3 3 3 1 1 1 b b c 
     c b b b 3 3 1 1 3 3 1 1 d d b c 
     c b b b b d d 1 1 3 b d d d b c 
-    . c b b b b d d b b b b b b c . 
-    . . c c b b b b b b b b c c . . 
-    . . . . c c c c c c c c . . . . 
-    . . . . . . b 1 1 b . . . . . . 
-    . . . . . . b 1 1 b b . . . . . 
-    . . . . . b b d 1 1 b . . . . . 
-    . . . . . b d d 1 1 b . . . . . 
+    f c b b b b d d b b b b b b c f 
+    f f c c b b b b b b b b c c f f 
+    f f f f c c c c c c c c f f f f 
+    f f f f f f b 1 1 b f f f f f f 
+    f f f f f f b 1 1 b b f f f f f 
+    f f f f f b b d 1 1 b f f f f f 
+    f f f f f b d d 1 1 b f f f f f 
     `, SpriteKind.Player)
+controller.moveSprite(nit1)
